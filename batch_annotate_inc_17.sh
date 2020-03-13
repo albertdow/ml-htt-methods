38e90f1c91d4fdc90c1f91ba9f7ba38e71e9c619
Albert <dow.albert@gmail.com>
2020-03-13 17:53:38 +0000

    Update batch annotate scripts to use no pT2-dijetpt training

--- batch_annotate_inc_17.sh
+++ batch_annotate_inc_17.sh
@@ -13,7 +13,7 @@ ulimit -c 0
 inputNumber=$(printf "%04d\n" $((SGE_TASK_ID-1)))
 
 # tt 
-python annotate_file_inc_17.py filelist/tmp_2017/tt/x${inputNumber} IC_12Mar2020 --model_folder ./data_tauspinner_2017 --training tauspinner --era 2017 --channel tt --config-training data_tauspinner_2017/tt_config_2017.yaml &> filelist/tmp_2017/tt/${inputNumber}.log 
+python annotate_file_inc_17.py filelist/tmp_2017/tt/x${inputNumber} IC_12Mar2020 --model_folder ./data_tauspinner_12Mar2020_2017_NopT2dijetpT/ --training tauspinner --era 2017 --channel tt --config-training data_tauspinner_12Mar2020_2017_NopT2dijetpT/tt_2017_config_inc.yaml &> filelist/tmp_2017/tt/${inputNumber}.log 
 
 # mt 
 # python annotate_file_inc_17.py filelist/tmp_2017/mt/x${inputNumber} IC_Nov25_tauspinner --model_folder ./data_tauspinner_2017 --training tauspinner --era 2017 --channel mt --config-training data_tauspinner_2017/mt_config_2017.yaml &> filelist/tmp_2017/mt/${inputNumber}.log 
--- batch_annotate_inc_18.sh
+++ batch_annotate_inc_18.sh
@@ -16,7 +16,7 @@ ulimit -c 0
 inputNumber=$(printf "%04d\n" $((SGE_TASK_ID-1)))
 
 # tt 
-python annotate_file_inc_18.py filelist/tmp_2018/tt/x${inputNumber} IC_12Mar2020 --model_folder ./data_tauspinner_2018 --training tauspinner --era 2018 --channel tt --config-training data_tauspinner_2018/tt_config_2018.yaml &> filelist/tmp_2018/tt/${inputNumber}.log 
+python annotate_file_inc_18.py filelist/tmp_2018/tt/x${inputNumber} IC_12Mar2020 --model_folder ./data_tauspinner_12Mar2020_2018_NopT2dijetpT/ --training tauspinner --era 2018 --channel tt --config-training data_tauspinner_12Mar2020_2018_NopT2dijetpT/tt_2018_config_inc.yaml &> filelist/tmp_2018/tt/${inputNumber}.log 
 
 # mt 
 # python annotate_file_inc_18.py filelist/tmp_2018/mt/x${inputNumber} IC_Nov25_tauspinner --model_folder ./data_tauspinner_2018 --training tauspinner --era 2018 --channel mt --config-training data_tauspinner_2018/mt_config_2018.yaml &> filelist/tmp_2018/mt/${inputNumber}.log 
